0dcf774473b8db05bef991d1dfb8ec4493c403eb
varun-s22 <93476421+varun-s22@users.noreply.github.com>
2022-04-27 01:20:19 +0530

    Live face recognition added

--- src/face_recognize.py
+++ src/face_recognize.py
@@ -2,36 +2,45 @@ import cv2 as cv
 import numpy as np
 import os
 
-celebs = []
-
-for folder in os.listdir("../training_images"):
-    if(folder[0] == "."):
-        continue
-    celebs.append(folder)
+persons = []
+parent="../photos"
 
 # loads up the trained data
+for folder in os.listdir(parent):
+    if(folder[0]=="."):
+        continue
+    persons.append(folder)
 cascade = cv.CascadeClassifier("../haarcascade_face.xml")
-faces = np.load("../faces.npy", allow_pickle=True)
-labels = np.load("../labels.npy")
+faces = np.load("../faces2.npy", allow_pickle=True)
+labels = np.load("../labels2.npy")
 
 # loads the trained .yml file
 recognizer = cv.face.LBPHFaceRecognizer_create()
-recognizer.read("../trainer.yml")
+recognizer.read("../trainerFaces.yml")
 
 # gets the image from the dir
-img = cv.imread("../untrained_images/taytay2.jpeg")
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-faces_detect = cascade.detectMultiScale(
-    gray_img, scaleFactor=1.1, minNeighbors=4)
+cap=cv.VideoCapture(0)
+while True:
+    isCaptured,img=cap.read()
+    if not isCaptured:
+        break
+    # img = cv.imread("../photos/45.jpg")
+    gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    faces_detect = cascade.detectMultiScale(
+        gray_img, scaleFactor=1.1, minNeighbors=4)
 
-# detects the face
-for(x, y, z, w) in faces_detect:
-    face_region = gray_img[y:y+w, x:x+z]
-    label, confidence = recognizer.predict(face_region)
-    cv.putText(img, str(celebs[label]), (500, 500), cv.FONT_HERSHEY_COMPLEX,
-               3.0, (0, 255, 0), thickness=5)
-    cv.rectangle(img, (x, y), (x+z, y+w), (0, 255, 0), thickness=8)
+    # detects the face
+    for(x, y, z, w) in faces_detect:
+        face_region = gray_img[y:y+w, x:x+z]
+        label, confidence = recognizer.predict(face_region)
+        if(confidence>=45):
+            cv.putText(img, str(persons[label]), (500, 500), cv.FONT_HERSHEY_COMPLEX,
+                    3.0, (0, 255, 0), thickness=5)
+        cv.rectangle(img, (x, y), (x+z, y+w), (0, 255, 0), thickness=8)
 
-# recognizes with help of model
-cv.imshow("unknown person", img)
-cv.waitKey(0)
+    # recognizes with help of model
+    cv.imshow("unknown person", img)
+    if(cv.waitKey(1)==ord("q")):
+        break
+cap.release()
+cv.destroyAllWindows()
--- src/face_train.py
+++ src/face_train.py
@@ -1,49 +1,68 @@
 import os
 import cv2 as cv
+import time
 import numpy as np
+
+def takePicture(folder_name):
+    # takes picture from the webcam
+    cap = cv.VideoCapture(0)
+    startTime = time.time()
+    ctr = 0
+    while int(time.time()-startTime) <= 6:
+        isCaptured, frame = cap.read()
+        if not isCaptured:
+            print("Can't receive frame ")
+            break
+        gray_img_obj = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        cv.imwrite(f"../photos/{folder_name}/{ctr}.jpg", gray_img_obj)
+        ctr += 1
+        cv.imshow("frame", frame)
+        if(cv.waitKey(1) == ord("q")):
+            break
+    cap.release()
+    cv.destroyAllWindows()
 
 # getting data
 labels = []
-celebs = []
+persons = []
 faces = []
 # loads a haarcascade.xml file (avaiable at opencv repo)
 cascade = cv.CascadeClassifier("../haarcascade_face.xml")
+folder_name=input("Enter folder name: ")
+parent="../photos"
+pathToFolder=os.path.join(parent,folder_name)
+os.mkdir(pathToFolder)
+takePicture(folder_name)
 
-for folder in os.listdir("../training_images"):
-    # gets all the data
-    if(folder[0] == "."):
+for folder in os.listdir(parent):
+    if(folder[0]=="."):
         continue
-    celebs.append(folder)
-
+    persons.append(folder)
 # detects faces on the data
-for folder in os.listdir("../training_images"):
+for folder in os.listdir(parent):
     if(folder[0] == "."):
         continue
-    label = celebs.index(folder)
-    dir_path = os.path.join("../training_images", folder)
-    for imgs in os.listdir(dir_path):
-        if(imgs[0] == "."):
-            continue
-        img_path = os.path.join(dir_path, imgs)
-        img_obj = cv.imread(img_path)
-        gray_img_obj = cv.cvtColor(img_obj, cv.COLOR_BGR2GRAY)
-        faces_detect = cascade.detectMultiScale(
-            gray_img_obj, scaleFactor=1.1, minNeighbors=4)
-
+    label=persons.index(folder)
+    folder_path = os.path.join(parent, folder)
+    for img in os.listdir(folder_path):
+        img_path=os.path.join(folder_path,img)
+        img_obj=cv.imread(img_path)
+        gray_img_obj=cv.cvtColor(img_obj,cv.COLOR_BGR2GRAY)
+        faces_detect = cascade.detectMultiScale(gray_img_obj, scaleFactor=1.1, minNeighbors=4)
         for(x, y, z, w) in faces_detect:
             face_region = gray_img_obj[y:y+w, x:x+z]
             # adds data to the list
             labels.append(label)
-            faces.append(face_region)
+            faces.append(face_region)  
 
-labels = np.array(labels)
-faces = np.array(faces, dtype="object")
+labels22 = np.array(labels)
+faces22 = np.array(faces, dtype="object")
 
 # trains the model from the data
 recognizer = cv.face.LBPHFaceRecognizer_create()
-recognizer.train(faces, labels)
+recognizer.train(faces22, labels22)
 
 # saves the trained model in .yml file
-recognizer.save("../trainer.yml")
-np.save("../faces.npy", faces)
-np.save("../labels.npy", labels)
+recognizer.save("../trainerFaces.yml")
+np.save("../faces2.npy", faces22)
+np.save("../labels2.npy", labels22)
